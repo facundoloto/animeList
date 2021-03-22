a53b462aba372e128b090f301e6b5bd34a2f0c7f
ema <lotofacundo6@gmail.com>
2021-03-22 13:03:22 -0300

    trabajando ui responsive

---
+++ js/home.js
@@ -0,0 +1,30 @@
+let anime=document.getElementById("home")
+
+/*es lo mismo que search*/
+async function peticionHome(container,idTemplate,idTitle,idImg,idRanking,idCalificacion,idDate,url){//top
+    let peticionHome=await fetch( `https://api.jikan.moe/v3/top/anime`)
+    let resultadoHome=await peticionHome.json()
+    console.log(resultadoHome)
+    for(let i=0;i<30;i++){
+    console.log(resultadoHome.top[`${i}`].title)
+    let fragment = document.createDocumentFragment();
+    const template=document.getElementById(idTemplate) //guardamos el temaplate en una variable
+    const newTemplate=template.content.cloneNode(true) //clonamos el template
+    newTemplate.getElementById(url).href=resultadoHome.top[`${i}`].url
+    newTemplate.getElementById(idTitle).textContent=`${i+1}`+'-'+`${resultadoHome.top[`${i}`].title}`
+    newTemplate.getElementById(idImg).src=`${resultadoHome.top[`${i}`].image_url}`
+    newTemplate.getElementById(idRanking).textContent=`${resultadoHome.top[`${i}`].score}`
+    newTemplate.getElementById(idCalificacion).textContent=`${resultadoHome.top[`${i}`].rated}`
+    let inicio=resultadoHome.top[`${i}`].start_date
+    let final=resultadoHome.top[`${i}`].end_date
+    let fechaInicio = new Date(Date.parse(inicio));
+    let fechaFinal = new Date(Date.parse(final));
+    newTemplate.getElementById(idDate).textContent=fechaInicio.getFullYear()+" / "+fechaFinal.getFullYear()
+    
+    console.log(resultadoHome.top[`${i}`].url)
+    fragment.appendChild(newTemplate) //fragment guarda todos los elementos para cuando queramos usarlos lo podamos mostrar en el dom cuando queramos
+    container.appendChild(fragment)
+    }
+  }
+
+peticionHome(anime,"template","title","img","ranking","calificacion","date","urlLink")
--- js/list.js
+++ js/list.js
@@ -1,27 +1,9 @@
 let tipo="anime" //esta variable va ser para cambiar el tipo de buscada en la peticion si sera anime o se buscara manga
 let animeButton=document.getElementById("searchButton")
 let input=document.getElementById("searchInput")
-let typeAnime=document.getElementById("anime")
-let typeManga=document.getElementById("manga")
 let anime=document.getElementById("listAnime")
 let flecha=document.getElementById("scroll")
 flecha.addEventListener("click",function (){window.scrollTo(0, 0)})
-
-typeAnime.addEventListener("click",function(){
-tipo="anime"
-remove(anime) //quita todo lo que este en el contenedor donde se mostra el template
-buscar(input.value,tipo,anime,"nombre","template","title","img","ranking","calificacion","date","synopsis")
-typeAnime.className = "btn btn-warning"; //activa y descativa el relleno dependiendo que opcion este activada
-typeManga.className="btn btn-outline-success"
-})
-
-typeManga.addEventListener("click",function(){
-tipo="manga"
-remove(anime)
-buscar(input.value,tipo,anime,"nombre","template","title","img","ranking","calificacion","date","synopsis")
-typeManga.className = "btn btn-success";
-typeAnime.className="btn btn-outline-warning"
-})
 
 
 input.addEventListener("keydown", (e)=> {
@@ -31,10 +13,5 @@ e.preventDefault()
 remove(anime)
 buscar(input.value,tipo,anime,"nombre","template","title","img","ranking","calificacion","date","synopsis")
 }
-})
-
-animeButton.addEventListener("click",()=>{
-remove(anime)
-buscar(input.value,tipo,anime,"nombre","template","title","img","ranking","calificacion","date","synopsis")
 })
 
