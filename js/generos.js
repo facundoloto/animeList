0df6c8c71960e775116ba66834870ac862c07e4d
ema <lotofacundo6@gmail.com>
2021-03-30 03:24:10 -0300

    funciones terminadas[C

--- js/generos.js
+++ js/generos.js
@@ -1,15 +1,26 @@
 let anime=document.getElementById("home")
 let next=document.getElementById("pageNext")
 let prev=document.getElementById("pagePrev")
-    /*es lo mismo que search*/
-    function remove(container){ //en container pasamos como parametro lo que vamos a borrar dentro de el
-      while (container.firstChild) { //si hay un hijo pasa al ciclo y elimana a todos hasta que el primero hijo de falso(ya no tenga ningun nodo hijo)
-      container.removeChild(container.firstChild); 
-      }
-      }
-    let page=1
-    async function peticionGeneros(page){//top
-        let peticionGeneros=await fetch( `https://api.jikan.moe/v3/search/anime?q=&page=${page}&genre=1`)
+let pagina=document.getElementById("pagina")
+let page=1
+let generoActual=1//el genero cambiara dependiendo el boton pero tener de manera global servira para cambiar de pagina y que esa funcion sepa en que generos estamos
+let shounen=document.getElementById("shounen")
+let sports=document.getElementById("sport")
+let romance=document.getElementById("romance")
+let school=document.getElementById("school")
+let policial=document.getElementById("policial")
+let Drama=document.getElementById("drama")
+//+18
+let horror=document.getElementById("horror")
+let seinen=document.getElementById("seinen")
+let echhi=document.getElementById("ecchi")
+let hentai=document.getElementById("hentai")
+let yuri=document.getElementById("yuri")
+let yaoi=document.getElementById("yaoi")
+
+    async function peticionGeneros(page,genero){//top
+        pagina.textContent=page//este valo cambiara de pagina y traera otro json
+        let peticionGeneros=await fetch( `https://api.jikan.moe/v3/search/anime?q=&page=${page}&genre=${genero}`)
         let resultadoGeneros=await peticionGeneros.json()
         console.log(resultadoGeneros)
         for(let i=0;i<10;i++){
@@ -32,24 +43,111 @@ let prev=document.getElementById("pagePrev")
         anime.appendChild(fragment)
         }
       }
+       
+          /*es lo mismo que search*/
+    function remove(container){ //en container pasamos como parametro lo que vamos a borrar dentro de el
+      while (container.firstChild) { //si hay un hijo pasa al ciclo y elimana a todos hasta que el primero hijo de falso(ya no tenga ningun nodo hijo)
+      container.removeChild(container.firstChild); 
+      }
+      }
+     
+       peticionGeneros(page,generoActual)//genero por defecto
+       
+       
+       shounen.addEventListener("click",function(){
+        generoActual=27
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
+       
+       school.addEventListener("click",function(){
+        generoActual=23
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
 
-    
-       peticionGeneros(page)
+       
+       romance.addEventListener("click",function(){
+        generoActual=22
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
 
+       
+       policial.addEventListener("click",function(){
+        generoActual=39
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
+
+       
+       drama.addEventListener("click",function(){
+        generoActual=8
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
+
+       
+       sports.addEventListener("click",function(){
+        generoActual=30
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
+
+       
+       horror.addEventListener("click",function(){
+        generoActual=14
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
+
+       
+       hentai.addEventListener("click",function(){
+        generoActual=12
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
+
+       
+       seinen.addEventListener("click",function(){
+        generoActual=42
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
+
+       
+       yuri.addEventListener("click",function(){
+        generoActual=34
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
+
+       
+       yaoi.addEventListener("click",function(){
+        generoActual=33
+        remove(anime)
+        peticionGeneros(page,generoActual)
+       })
+
+  
        next.addEventListener("click",function(){
-        //hacer un if para que sume la cant de pagina max que tiene la api y no sume mas  que eso
-         page+=1
-         console.log(page)
-         remove(anime)
-         peticionGeneros(page)
+        //avanza hasta la pagina 20 //hacer if para desactivar next si estamos en la pagina 20
+        if(page<=20){
+          page+=1
+        remove(anime)
+        peticionGeneros(page,generoActual)
+         
+        }
        })
        
        prev.addEventListener("click",function(){
-        //if para que retroceda solo hasta que la pag sea 1 nomas
-        page-=1
-        console.log(page)
-        remove(anime)
-        peticionGeneros(page)
+        //si estamos en la pagina 1 desactivar prev
+        if(page>1){
+          page-=1
+          remove(anime)
+          peticionGeneros(page,generoActual)
+        }
+      
       })
 
       //obtener la cantidad de paginas desde el resultado de la api para saber cuantas paginas tiene para cambiar y ponerle un tope
